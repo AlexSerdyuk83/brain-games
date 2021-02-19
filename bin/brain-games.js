3471c406fc6ca093ece31b3ff19bbb825a32d9af
Aleksandr Serdyuk <a.galko83@gmail.com>
2021-02-19 14:00:15 +0300

    feat: add user's greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+import { great } from '/home/aleksandr/projects/frontend-project-lvl1/src/cli.js';
 
 const GREAT = "Welcome to the Brain Games!"
+
 console.log(GREAT);
+
+great();
